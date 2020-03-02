828bf4b5267b906f108e7835f1cdab4d14ec669c
Oleg <olegin64@gmail.com>
2020-03-02 22:41:15 +0200

    code clean

--- src/app/models/post.model.ts
+++ src/app/models/post.model.ts
@@ -1,7 +1,17 @@
+import { Injectable } from '@angular/core';
+import {Adapter} from '../adapter/adapter';
+
 export class PostModel {
   constructor(
     public id: string | null = null,
     public title: string | null = null,
     public body: string | null = null
   ) { }
+}
+
+@Injectable({ providedIn: 'root' })
+export class PostAdapter implements Adapter<PostModel> {
+  adapt(item: any): PostModel {
+    return new PostModel(item.id, item.title, item.body);
+  }
 }
--- src/app/users/users.service.ts
+++ src/app/users/users.service.ts
@@ -3,7 +3,7 @@ import {HttpClient} from '@angular/common/http';
 import {Observable} from 'rxjs';
 import {UserModel} from '../models/user.model';
 import {map} from 'rxjs/operators';
-import {PostAdapterService} from '../adapter/post-adapter.service';
+import { PostAdapter } from '../models/post.model';
 
 @Injectable({
   providedIn: 'root'
@@ -11,14 +11,14 @@ import {PostAdapterService} from '../adapter/post-adapter.service';
 export class UsersService {
   constructor(
     private httpClient: HttpClient,
-    private postAdapter: PostAdapterService
+    private postAdapter: PostAdapter
   ) { }
   // Static class method adapter
   getUsers(): Observable<UserModel[]> {
     return this.httpClient.get('https://jsonplaceholder.typicode.com/users')
       .pipe(map((data: any[]) => data.map(UserModel.adapt)));
   }
-  // Service class adapter
+  // Service injectable model class adapter
   getPosts() {
     return this.httpClient.get('https://jsonplaceholder.typicode.com/posts/1')
       .pipe(map((data: any) => this.postAdapter.adapt(data)));
